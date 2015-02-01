4dd1fb4717903118be17207fd26b86de39c2474d
Martin Kikegawa <mkikegawa@gmail.com>
2015-02-01 17:59:03 -0500

    reworked app.rb based on Rubocop

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-require "sinatra"
-require_relative "./model/library"
+require 'sinatra'
+require_relative './model/library'
 
 get '/' do
   @active = 'home'
@@ -7,38 +7,35 @@ get '/' do
 end
 
 get '/movies' do
-  @category   = params[:category] || "title"
-  @sort_order = params[:sort_order] || "asc"
+  @category   = params[:category] || 'title'
+  @sort_order = params[:sort_order] || 'asc'
   @search     = params[:search]
   @active     = 'movies'
-   
-  if @category || @search 
-    @library  = Library.movies_search_sort(@search, @category, @sort_order)
+  if @category || @search
+    @library = Library.movies_search_sort(@search, @category, @sort_order)
   else
-    @library  = Library.all
+    @library = Library.all
   end
   erb :movies
 end
 
 get '/movies/:id' do
   @active = 'movies'
-  @search     = params[:search]
+  @search = params[:search]
   if !@search
     @movie = Library.find_by_id(params[:id].to_i)
     erb :movie
-  else 
-    redirect to("/movies?search=#{ @search }") 
+  else
+    redirect to("/movies?search=#{ @search }")
   end
 end
 
 get '/about' do
-  @active ='about'
-  @search     = params[:search]
+  @active = 'about'
+  @search = params[:search]
   if !@search
     erb :about
   else
-    redirect to("/movies?search=#{ @search }") 
+    redirect to("/movies?search=#{ @search }")
   end
 end
-
-
